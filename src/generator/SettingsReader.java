2de9edba6c6000869a4c483f7dc2f943b049d25e
Blicker777 <lordid@rambler.ru>
2017-08-12 14:57:14 +0300

    Созданы поля присваивающие значения исходных данных, созданы листы Int и String значений, а также getter всех int полей

--- src/generator/SettingsReader.java
+++ src/generator/SettingsReader.java
@@ -1,5 +1,23 @@
 package generator;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SettingsReader {
+
+    private String adress;
+
+    private int width;
+    private int height;
+    private String number;
+    private String date;
+    private String fio;
+    private int numberVal;
+    private int dateVal;
+    private int fioVal;
+
+    List<Integer> listInt = new ArrayList<>();
+    List<String> listStr = new ArrayList<>();
+
 }
