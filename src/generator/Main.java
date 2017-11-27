44544a329801140c8656182e0f1b7a6979454249
Blicker777 <lordid@rambler.ru>
2017-11-27 20:06:49 +0300

    Доработан класс main

--- src/generator/Main.java
+++ src/generator/Main.java
@@ -8,5 +8,11 @@ public class Main {
         SettingsReader reader = new SettingsReader();
         reader.fileReader();
         System.out.println(reader);
+
+        DateReader dateReader = new DateReader("source-data.tsv");
+        System.out.println(dateReader.fileReaderString());
+
+        Generator generator = new Generator("result.txt");
+        generator.generatorText(dateReader, reader);
     }
 }
